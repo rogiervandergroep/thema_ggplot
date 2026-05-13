e4ea0c4e2fc78034c1bfd7eefe01b53600d3fcea
rogiervandergroep <rogier.van.der.groep@amsterdam.nl>
2026-05-13 20:58:15 +0200

    invoegen nieuw script met voorbeeld

--- 01_scripts/script_ggplot_voorbeeld.R
+++ 01_scripts/script_ggplot_voorbeeld.R
@@ -1,107 +1,20 @@
 # Dataframe aanmaken in R
 
+## lees alle ggplot scripts in
 source(
   "https://raw.githubusercontent.com/rogiervandergroep/thema_ggplot/main/01_scripts/script_ggplot_theme.R"
 )
 
-
-data_voorbeeld <- data.frame(
-  week = c(
-    "week 1",
-    "week 1",
-    "week 1",
-    "week 1",
-    "week 1",
-    "week 1",
-    "week 1",
-    "week 1",
-    "week 1",
-    "week 1",
-    "week 2",
-    "week 2",
-    "week 2",
-    "week 2",
-    "week 2",
-    "week 2",
-    "week 2",
-    "week 2",
-    "week 2",
-    "week 2"
-  ),
-
-  dag = c(
-    "ma",
-    "ma",
-    "di",
-    "di",
-    "wo",
-    "wo",
-    "do",
-    "do",
-    "vrij",
-    "vrij",
-    "ma",
-    "ma",
-    "di",
-    "di",
-    "wo",
-    "wo",
-    "do",
-    "do",
-    "vrij",
-    "vrij"
-  ),
-
-  meetmoment = c(
-    "1e keer",
-    "2e keer",
-    "1e keer",
-    "2e keer",
-    "1e keer",
-    "2e keer",
-    "1e keer",
-    "2e keer",
-    "1e keer",
-    "2e keer",
-    "1e keer",
-    "2e keer",
-    "1e keer",
-    "2e keer",
-    "1e keer",
-    "2e keer",
-    "1e keer",
-    "2e keer",
-    "1e keer",
-    "2e keer"
-  ),
-
-  waarde = c(
-    5,
-    5,
-    6,
-    6,
-    7,
-    7,
-    8,
-    8,
-    18,
-    18,
-    9,
-    9,
-    10,
-    10,
-    11,
-    11,
-    12,
-    12,
-    13,
-    13
-  )
+## een voorbeeldtabel
+source(
+  "https://raw.githubusercontent.com/rogiervandergroep/thema_ggplot/main/01_scripts/script_ggplot_tabel.R"
 )
 
-
+# fixeer de weekdagen anders krijg je  alfabetische volgorde
 dag_levels <- c("ma", "di", "wo", "do", "vrij", "za", "zo")
 
+
+## data met vertikale kolommen
 data_voorbeeld |>
 
   my_plot_vertical(
@@ -112,7 +25,19 @@ data_voorbeeld |>
   ) +
   facet_wrap(~week)
 
+### data zonder facet
+data_voorbeeld |>
+  filter(week == 'week 1') |>
 
+  my_plot_vertical(
+    y_var = waarde,
+    x_var = fct_relevel(dag, dag_levels),
+    fill_var = fct_rev(meetmoment),
+    color_var = blauw_pal[c(1, 6)]
+  )
+
+
+## figuur met horizontale kolommen
 data_voorbeeld |>
 
   my_plot_horizontal(
